8351eb7dc28238bb75060a1aa83b5cdca48f2b42
ckgovind <gsajeev2014@gmail.com>
2026-05-02 21:13:03 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.go
@@ -0,0 +1,11 @@
+func hasDuplicate(nums []int) bool {
+    hashMap := make(map[int]bool)
+	for _,num := range nums{
+		if _,ok := hashMap[num]; ok{
+			return true
+		}
+		hashMap[num] = true
+	}
+
+	return false
+}

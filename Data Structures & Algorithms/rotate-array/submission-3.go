22cd8b6c37acf22055b00b1d9842456c994de8d5
ckgovind <gsajeev2014@gmail.com>
2026-05-28 20:40:14 +0530

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.go
@@ -0,0 +1,22 @@
+func rotate(nums []int, k int) {
+
+	k = k % len(nums)
+	var count int
+
+	for start := 0; count < len(nums); start++ {
+		curr := start
+		var temp, prev int
+		prev = nums[curr]
+		for {
+			nxtindx := (curr + k) % len(nums)
+			temp = nums[nxtindx]
+			nums[nxtindx] = prev
+			prev = temp
+			curr = nxtindx
+			count++
+			if curr == start {
+				break
+			}
+		}
+	}
+}
